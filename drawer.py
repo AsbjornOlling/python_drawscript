21741aa01b30a7750213bfde2eec7577d8328bb8
AsbjornOlling <asbjornolling@gmail.com>
2017-10-16 23:08:33 +0200

    Fixed height issues - added saveFrame function

--- drawer.py
+++ drawer.py
@@ -3,26 +3,26 @@
 from PIL import Image, ImageDraw
 import random
 import math
-from copy import deepcopy
 
-debug = True
-
-
-# makes a blank imagefile with the same size a source image
-def initImg(filename):
-    newImg = Image.new("RGB",srcImg.size,"white")
-    newImg.save(filename)
-    return newImg
+debug = False
+saveframes = True
+iterations = 100000
+inputfile = "./fjong2.png"
+outputfile = "./output.png"
 
 
 # draw a random square
 def drawRandomSquare():
+
+    # init drawer class
+    draw = ImageDraw.Draw(drawImg)
+
     width, height = drawImg.size
 
     global size
-    size = 2
+    size = random.randint(1,5)
 
-    p1 = (random.randint(0,width),random.randint(0,width))
+    p1 = (random.randint(0,width),random.randint(0,height))
     # p2 is "just outside" rectangle
     p2 = (p1[0]+size,p1[1]+size) 
    
@@ -30,7 +30,13 @@ def drawRandomSquare():
     global bounds
     bounds = (p1[0],p1[1],p2[0],p2[1]) 
 
-    # get random color
+    # handle bounds outside box
+    if bounds[2] > width or bounds[3] > height:
+        bounds = (p1[0],p1[1],width,height) 
+        if debug == True:
+            print("Changing bounds")
+
+    # get jandom color
     global srcColors
     color = srcColors[random.randint( 0, len(srcColors)-1 )]
 
@@ -38,8 +44,8 @@ def drawRandomSquare():
     draw.rectangle([p1,p2],fill=color)
 
     if debug == True:
-        # save image
-        drawImg.save("img_finale.png")
+        # save image after draw
+        drawImg.save(outputfile)
         input()
 
 
@@ -76,14 +82,20 @@ def compareWithSrc(img):
     global bounds
 
     # crop images
-    imgCrop = deepcopy(img)
-    srcCrop = deepcopy(srcImg)
+    imgCrop = img.copy()
+    srcCrop = srcImg.copy()
     imgCrop = imgCrop.crop( bounds )
     srcCrop = srcCrop.crop( bounds )
 
-    # sanity check for size
-    if (debug == True):
+    # make list of pixels in image
+    imgCropList = list(imgCrop.getdata())
+    srcCropList = list(srcCrop.getdata())
+
+    if debug == True:
+        # print size and crop size
         global size
+        print("Size: "+str(size)+" ImgCrop: "+str(imgCrop.size)+" SrcCrop: "+str(srcCrop.size)+"Bounds: "+str(bounds) )
+        # sanity check for size
         imgW, imgH = imgCrop.size
         if (imgW != size or imgH != size):
             print("Size problem with img")
@@ -94,57 +106,59 @@ def compareWithSrc(img):
             print("Size problem with src")
             print("Width: "+str(srcW)+" Height: "+str(srcH))
             print("Size is set to: "+str(size))
-
-    # make list of pixels in image
-    imgCropList = list(imgCrop.getdata())
-    srcCropList = list(srcCrop.getdata())
-    
-    if (debug == True):
         # sanity check list length
         if ( len(imgCropList) != len(srcCropList)):
             print("Pixel list lengths not equal.")
-        imgCrop.save("imgCrop.png")
-        srcCrop.save("srcCrop.png")
+        # save crops every time
+        #imgCrop.save("imgCrop.png")
+        #srcCrop.save("srcCrop.png")
 
     # init diff
     diff = 0
 
-    # loop though list 
+    # loop though list, calculate manhattan color distance
     for i in range( 0, len(imgCropList) ):
-        # calculate manhattan color distance
         diffR = abs(imgCropList[i][0] - srcCropList[i][0])
         diffG = abs(imgCropList[i][1] - srcCropList[i][1])
         diffB = abs(imgCropList[i][2] - srcCropList[i][2])
         diff += diffR + diffG + diffB
 
-    if diff == 0:
-        print("Nulldiff.")
-
     return diff
 
+# number and save output image in folder
+def saveFrame(i):
+    if i > 9999:
+        zeroes = ""
+    elif i > 999:
+        zeroes = "0"
+    elif i > 99:
+        zeroes = "00"
+    elif i > 9:
+        zeroes = "000"
+    else:
+        zeroes = "0000"
+    framefile = "image_"+zeroes+str(i)
+    drawImg.save("./out/"+framefile+".png")
 
 # load source image
-filename = "./tux.png"
-srcImg = Image.open(filename)
+srcImg = Image.open(inputfile)
 analyzeSource()
 
-# make new blank image and a copy of it
-compImg = initImg("new_img.png")
-drawImg = deepcopy(compImg)
+# make new blank image and a copy of it, don't save shit
+compImg = Image.new("RGB",srcImg.size,"white")
+drawImg = compImg.copy()
 
-# init drawer class
-draw = ImageDraw.Draw(drawImg)
-
-####
-# GO GO GO
+    ####
+    # GO GO GO
 
 # declare the counter vars
 accept = 0
 reject = 0
 identical = 0
-for i in range(0,1000000):
+framecount = 0
+for i in range(0,iterations):
 
-    #STATS
+    # MAIN STATS LINE
     print("Drawing square no: "+str(i)+" (Accepted: "+str(accept)+" Rejected: "+str(reject)+" Identical: "+str(identical)+")")
 
     # draw another one
@@ -162,13 +176,13 @@ for i in range(0,1000000):
     # if new image is better:
     if ( drawImgDiff < compImgDiff ):
         # make new comp, and transfer diff value
-        compImg = deepcopy(drawImg)
+        compImg = drawImg.copy()
         accept += 1
 
     # if new image is worse
     elif ( drawImgDiff > compImgDiff ):
         # reset
-        drawImg = deepcopy(compImg)
+        drawImg = compImg.copy()
         reject += 1
 
     else:
@@ -176,8 +190,12 @@ for i in range(0,1000000):
         identical += 1
 
     # make new file every tenth iteration
-    if (accept % 10 == 0 or debug == True):
-        drawImg.save("img_finale.png")
+    if (accept % 100 == 0 or debug == True):
+        if saveframes == True:
+            saveFrame(framecount)
+            framecount += 1
+        else:
+            drawImg.save(outputfile)
 
-# save when finished
-drawImg.save("img_finale.png")
+#save when finished
+drawImg.save(outputfile)
